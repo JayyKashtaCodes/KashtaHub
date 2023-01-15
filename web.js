dfb8c601e19fd7228f26ca8c0e35531a64b53444
Jayy von Kush <jtman112@gmail.com>
2023-01-16 10:47:14 +1300

    web.js

--- web.js
+++ web.js
@@ -1,3 +1,4 @@
+    //Auto Locale
 $( document ).ready(function(){
     var userLang = navigator.language || navigator.userLanguage;
     if (userLang == "en") {
@@ -10,3 +11,41 @@ $( document ).ready(function(){
         window.location.href = "/KashtaHub/"
     }
 });
+
+// Cache
+
+var solutions = [
+    function () { location.reload(); },
+    function () { history.go(0); },
+    function () { location.href = location.href; },
+    function () { location.href = location.pathname; },
+    function () { location.replace(location.pathname); },
+    function () { location.reload(forceGet); },
+];
+
+$("[data-func]").on("click", function () {
+    solutions[parseInt($(this).attr("data-func"))]();
+});
+
+
+ // $('#something').click(function() {
+ //   location.reload(true);
+ // });
+
+
+ // button stuff
+ var methods = [
+    "#",
+    "#"
+  ];
+  
+var $body = $("body");
+for (var i = 0; i < methods.length; ++i) {
+  (function(cMethod) {
+    $body.append($("<button>", {
+      text: cMethod
+    }).on("click", function() {
+      eval(cMethod); // don't blame me for using eval
+    }));
+  })(methods[i]);
+}
