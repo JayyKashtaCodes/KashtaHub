7bc42ee7371401e3aa3290ef8b7284f987ab1f2f
Jayy von Kush <jtman112@gmail.com>
2023-01-16 11:05:49 +1300

    update

--- web.js
+++ web.js
@@ -11,36 +11,3 @@ $( document ).ready(function(){
         window.location.href = "/KashtaHub/"
     }
 });
-
-// Cache
-
-var solutions = [
-    function () { location.reload(forceGet); },
-];
-
-$("[data-func]").on("click", function () {
-    solutions[parseInt($(this).attr("data-func"))]();
-});
-
-
- // $('#something').click(function() {
- //   location.reload(true);
- // });
-
-
- // button stuff
- var methods = [
-    "#",
-    "#"
-  ];
-  
-var $body = $("body");
-for (var i = 0; i < methods.length; ++i) {
-  (function(cMethod) {
-    $body.append($("<button>", {
-      text: cMethod
-    }).on("click", function() {
-      eval(cMethod); // don't blame me for using eval
-    }));
-  })(methods[i]);
-}
