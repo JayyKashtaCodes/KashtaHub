d3247a2f26d11e6216508e81197451b7b1b36c31
Jayy von Kush <jtman112@gmail.com>
2023-01-25 00:14:56 +1300

    font

--- en/js/web2.js
+++ en/js/web2.js
@@ -2,3 +2,8 @@ window.onload = function()
 {
   document.getElementById("spanYear").innerHTML = new Date().getFullYear();
 }
+
+document.fonts.ready().then(function() {
+  var content = document.getElementById("content");
+  content.style.visibility = "visible";
+});
